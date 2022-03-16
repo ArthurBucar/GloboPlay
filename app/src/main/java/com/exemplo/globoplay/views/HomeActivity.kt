892f6dde4a03c1bf5caf7b2072995d45b14c08cb
arthurbucarcavalcante <arthur.bucar@dextra-sw.com>
2022-03-16 11:08:50 -0300

    change layout

--- app/src/main/java/com/exemplo/globoplay/views/HomeActivity.kt
+++ app/src/main/java/com/exemplo/globoplay/views/HomeActivity.kt
@@ -1,14 +1,23 @@
 package com.exemplo.globoplay.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.exemplo.globoplay.Movie
+import com.exemplo.globoplay.R
 import com.exemplo.globoplay.adapters.MoviesAdapter
 import com.exemplo.globoplay.repository.MoviesRepository
-import com.exemplo.globoplay.R
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var popularMovies: RecyclerView
@@ -16,6 +25,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setTitle("globoplay")
 
         popularMovies = findViewById(R.id.popular_movies)
         popularMovies.layoutManager = LinearLayoutManager(
@@ -30,6 +40,36 @@ class HomeActivity : AppCompatActivity() {
             onSuccess = ::onPopularMoviesFetched,
             onError = ::onError
         )
+    }
+
+    private fun setTitle(title: String?) {
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val textView = TextView(this)
+        textView.text = title
+        textView.textSize = 26f
+        textView.setTypeface(null, Typeface.BOLD)
+        textView.layoutParams =
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        textView.gravity = Gravity.CENTER
+        textView.setMargins(16, 16, 16, 16)
+        textView.setTextColor(resources.getColor(R.color.white))
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.customView = textView
+    }
+
+    fun View.setMargins(
+        left: Int = this.marginLeft,
+        top: Int = this.marginTop,
+        right: Int = this.marginRight,
+        bottom: Int = this.marginBottom,
+    ) {
+        layoutParams = (layoutParams as ViewGroup.MarginLayoutParams).apply {
+            setMargins(left, top, right, bottom)
+        }
     }
 
     private fun onPopularMoviesFetched(movies: List<Movie>) {
